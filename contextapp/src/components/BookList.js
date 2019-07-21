90a555e6dd6785ebab5f8d4173bc0371bf8f51fb
truong.dinh <dinhductruong24@gmail.com>
2019-07-21 11:58:59 +0700

    Init

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
-  render() { 
-    return ( 
-      <div className="book-list">
-        <ul>
-          <li>the way of kings</li>
-          <li>the name of the wind</li>
-          <li>the final empire</li>
-        </ul>
-      </div>
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ background: theme.bg, color: theme.syntax }}
+            >
+              <ul>
+                <li style={{ backgroundColor: theme.ui }}>the way of kings</li>
+                <li style={{ backgroundColor: theme.ui }}>
+                  the name of the wind
+                </li>
+                <li style={{ backgroundColor: theme.ui }}>the final empire</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
- 
+
 export default BookList;
